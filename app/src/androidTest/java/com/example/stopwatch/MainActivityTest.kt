4e9ade22daab6b89710fba91c2890d15931c0c27
Mr. Fox <m1sterfinch@yandex.ru>
2022-10-07 15:43:30 +0300

    test init

---
+++ app/src/androidTest/java/com/example/stopwatch/MainActivityTest.kt
@@ -0,0 +1,19 @@
+package com.example.stopwatch
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @Test
+    fun checkActivityVisibility() {
+
+    }
+
+}
--- app/src/test/java/com/example/stopwatch/ExampleUnitTest.kt
+++ app/src/test/java/com/example/stopwatch/ExampleUnitTest.kt
@@ -3,15 +3,25 @@ package com.example.stopwatch
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ExampleUiTestWithAndroidComposeRule {
+
+    @Rule
+    @JvmField
+    var composeTestRule: ComposeContentTestRule = createAndroidComposeRule<UiTestingDemoActivity>()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun whenIClickOnButton_TheTextShouldChange() {
+        composeTestRule.onNodeWithText("Hello").assertExists()
+        composeTestRule.onNodeWithText("Hello").performClick()
+        composeTestRule.onNodeWithText("Hello").assertDoesNotExist()
+        composeTestRule.onNodeWithText("Bye").assertExists()
     }
+
 }
